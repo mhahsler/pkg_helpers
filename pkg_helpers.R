716a57f7d641f105454ad4937e4022b9c22daea9
Michael Hahsler <michael@hahsler.net>
2022-06-26 17:32:15 -0500

    Create pkg_helpers.R

---
+++ pkg_helpers.R
@@ -0,0 +1,23 @@
+pkg_title <- function(pkg) {
+  title <- packageDescription(pkg)$Title
+  
+  options(digits = 2)
+  knitr::opts_chunk$set(tidy = TRUE, message = FALSE, fig.path = 'inst/README_files/')
+  
+  library(stringr)
+  
+  cat(str_interp('# <img src="man/figures/logo.svg" align="right" height="139" /> R package ${pkg} - ${title}\n\n'))
+  
+  cat(str_interp("[![CRAN version](http://www.r-pkg.org/badges/version/${pkg})](https://CRAN.R-project.org/package=${pkg})\n"))
+  cat(str_interp("[![stream r-universe status](https://mhahsler.r-universe.dev/badges/${pkg})](https://mhahsler.r-universe.dev/ui#package:${pkg})\n"))
+  cat(str_interp("[![CRAN RStudio mirror downloads](http://cranlogs.r-pkg.org/badges/${pkg})](https://CRAN.R-project.org/package=${pkg})\n"))
+}
+
+pkg_install <- function(pkg) {
+  cat('## Installation\n\n')
+  cat('__Stable CRAN version:__ Install from within R with\n')
+  cat(str_interp('```r\ninstall.packages("${pkg}")\n```\n'))
+  cat('__Current development version:__\n') 
+  cat(str_interp('Install from [r-universe.](https://mhahsler.r-universe.dev/ui#package:${pkg})\n'))
+  cat(str_interp('```r\ninstall.packages("${pkg}", repos = "https://mhahsler.r-universe.dev")\n```'))
+}
